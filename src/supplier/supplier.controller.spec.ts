fc62b283c285085bc2fe45da602245dee611be62
chooxingyu <chooxingyu@hotmail.com>
2023-04-16 15:51:33 +0800

    add in memory database to supplier tests

--- src/supplier/supplier.controller.spec.ts
+++ src/supplier/supplier.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupplierController } from './supplier.controller';
 import { SupplierService } from './supplier.service';
+import { SupplierModule } from './supplier.module';
+import { Supplier } from './entities/supplier.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('SupplierController', () => {
   let controller: SupplierController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          dropSchema: true,
+          entities: [Supplier],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Supplier]),
+      ],
       controllers: [SupplierController],
       providers: [SupplierService],
     }).compile();
--- src/supplier/supplier.service.spec.ts
+++ src/supplier/supplier.service.spec.ts
@@ -1,11 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupplierService } from './supplier.service';
+import { Supplier } from './entities/supplier.entity';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
 
 describe('SupplierService', () => {
   let service: SupplierService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          dropSchema: true,
+          entities: [Supplier],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([Supplier]),
+      ],
       providers: [SupplierService],
     }).compile();
 
