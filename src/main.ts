ec234fe71c177e9f6337043b26036b5ed7f21fb8
chooxingyu <chooxingyu@hotmail.com>
2023-04-16 20:53:37 +0800

    add forbidnonwhitelist and whitelisting

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }));
   const config = app.get(ConfigService);
   const PORT = config.getOrThrow('PORT');
 
