d21f5fc553866ae32452a4881c5f0c42715c5c1c
chooxingyu <chooxingyu@hotmail.com>
2023-04-16 13:57:34 +0800

    add typeorm support and init with data source configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from '../db/data-source';
 
 @Module({
   imports: [
@@ -14,6 +16,7 @@ import * as Joi from 'joi';
         PORT: Joi.number().default(3000),
       }),
     }),
+    TypeOrmModule.forRoot(dataSourceOptions),
   ],
   controllers: [AppController],
   providers: [AppService],
