04b54eb4f6601df2391ecd85e887e6ba870b40f1
chooxingyu <chooxingyu@hotmail.com>
2023-04-16 13:21:24 +0800

    add env config and JOI validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('dev', 'prod')
+          .default('dev'),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
