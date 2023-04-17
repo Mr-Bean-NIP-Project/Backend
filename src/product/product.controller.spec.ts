137b26c6966974ec41a814fa5e3b4bced40f87cf
chooxingyu <chooxingyu@hotmail.com>
2023-04-17 22:09:41 +0800

    add fix to product tests

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductController } from './product.controller';
-import { ProductService } from './product.service';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 import { TYPEORM_TEST_IMPORTS } from '../common/typeorm_test_helper';
 import { MaterialModule } from '../material/material.module';
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
 describe('ProductController', () => {
   let controller: ProductController;
 
+  beforeAll(() => {
+    initializeTransactionalContext();
+  });
+
   beforeEach(async () => {
+    jest.mock('typeorm-transactional', () => ({
+      Transactional: () => jest.fn(),
+    }));
     const module: TestingModule = await Test.createTestingModule({
       imports: [...TYPEORM_TEST_IMPORTS(), MaterialModule],
       controllers: [ProductController],
--- src/product/product.service.spec.ts
+++ src/product/product.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductService } from './product.service';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 import { TYPEORM_TEST_IMPORTS } from '../common/typeorm_test_helper';
 import { MaterialModule } from '../material/material.module';
+import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
+  beforeAll(() => {
+    initializeTransactionalContext();
+  });
 
   beforeEach(async () => {
+    jest.mock('typeorm-transactional', () => ({
+      Transactional: () => jest.fn(),
+    }));
     const module: TestingModule = await Test.createTestingModule({
       imports: [...TYPEORM_TEST_IMPORTS(), MaterialModule],
       providers: [ProductService],
