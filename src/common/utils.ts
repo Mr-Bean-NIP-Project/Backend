77a551b170d82963cc2069226ef5f8d39c66bce6
chooxingyu <chooxingyu@hotmail.com>
2023-04-18 17:54:20 +0800

    fix, TODO testing

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,10 +1,10 @@
-import Big from 'big.js';
+import Big, { BigSource } from 'big.js';
 
-export function sumArrayOfObjects<T>(objects: Array<T>): T {
+export function sumArrayOfObjects<T>(objects: Array<T | any>): T {
   if (!objects || objects.length == 0) return {} as T;
   return objects.reduce((a, obj) => {
     Object.entries(obj).forEach(([key, val]) => {
-      a[key] = Big(a[key] || 0).add(val);
+      a[key] = Big(a[key] || 0).add(val as BigSource);
     });
     return a;
   });
