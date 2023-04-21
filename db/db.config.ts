183aa4a9d404c9349a07fb16bc03c00a9c279fbc
chooxingyu <chooxingyu@hotmail.com>
2023-04-21 12:46:03 +0800

    change from specify db name to specify db path

--- db/db.config.ts
+++ db/db.config.ts
@@ -1,10 +1,14 @@
 import { registerAs } from '@nestjs/config';
 import { DataSourceOptions } from 'typeorm';
 
+const SQLIITE = 'sqlite';
+
 export default registerAs('database', () => {
   return {
-    type: 'sqlite',
-    database: `./${process.env.DATABASE_NAME}.sqlite`,
+    type: SQLIITE,
+    database: process.env.DATABASE_PATH.endsWith(`.${SQLIITE}`)
+      ? process.env.DATABASE_PATH
+      : `${process.env.DATABASE_PATH}.${SQLIITE}`,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
   } as DataSourceOptions;
