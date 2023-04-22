2d1b33ad7f76708d45ac9d7f27cb05f43effaafa
Raine <77650951+yuyitan@users.noreply.github.com>
2023-04-22 21:47:33 +0800

    add cors config

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,14 @@ async function bootstrap() {
   const PORT = config.getOrThrow('PORT');
 
   console.log(`Starting on Port: ${PORT}`);
+  const cors = require('cors');
+  const corsOptions = {
+    origin: 'http://localhost:3001',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
   await app.listen(PORT);
 }
 bootstrap();
